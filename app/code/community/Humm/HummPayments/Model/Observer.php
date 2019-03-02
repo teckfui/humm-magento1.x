4f584d52420cbcf5dd59ec78a29d7093b780de8a
Tao Wang <hellotaotao@gmail.com>
2019-03-02 10:58:25 +1030

    some changes in carry over values:
    1. don't carry over gateway_url as it can be deprecated but takes priority;
    2. set approved order status to 'processing' by default;
    3. set 'is_testing' to false by default

--- app/code/community/Humm/HummPayments/Model/Observer.php
+++ app/code/community/Humm/HummPayments/Model/Observer.php
@@ -51,11 +51,10 @@ class Humm_HummPayments_Model_Observer {
         if ( ! Mage::getStoreConfig( 'payment/HummPayments/merchant_number' ) ) {
             $is_carried_over = Mage::getStoreConfig( 'payment/HummPayments/settings_carried_over' );
             if ( Mage::getStoreConfig( 'payment/OxiPayments/merchant_number' ) && empty( $is_carried_over ) ) {
-                $carry_over_list = [
+                $carry_over_targets = [
                     'active',
                     'merchant_number',
                     'api_key',
-                    'gateway_url',
                     'is_testing',
                     'humm_approved_order_status',
                     'automatic_invoice',
@@ -65,10 +64,16 @@ class Humm_HummPayments_Model_Observer {
                     'specificcountry',
                     'sort_order',
                 ];
-                foreach ( $carry_over_list as $target ) {
+                foreach ( $carry_over_targets as $target ) {
                     $source = ( $target == 'humm_approved_order_status' ) ? 'oxipay_approved_order_status' : $target;
                     Mage::getConfig()->saveConfig( 'payment/HummPayments/' . $target, Mage::getStoreConfig( 'payment/OxiPayments/' . $source ) );
                 }
+                // set approved order status to 'processing' by default because other statuses may not exist
+                Mage::getConfig()->saveConfig( 'payment/HummPayments/humm_approved_order_status', 'processing' );
+                // 'is_testing' is a new setting so it does not exist in earlier versions.
+                // So setting it to 0 by default to avoid going into sandbox
+                // This is different from the default value in config.xml because we want new merchants to do testing first but old merchants should remain in live.
+                Mage::getConfig()->saveConfig( 'payment/HummPayments/is_testing', '0' );
             }
 
             Mage::getConfig()->saveConfig( 'payment/HummPayments/settings_carried_over', 1 );
